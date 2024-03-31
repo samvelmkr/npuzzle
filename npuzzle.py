58f046c130424f68054df975bfee72ca59f3f845
Samvel <samvel.mkrtchyan.proff.acc@gmail.com>
2024-03-31 19:42:18 +0400

    Init

---
+++ npuzzle.py
@@ -0,0 +1,122 @@
+from enum import Enum
+from search import *
+
+class Tiles(State):
+    EMPTY_TILE = 0
+
+    def __init__(self, tiles = None):
+        if tiles:
+            self.width = len(tiles)
+            self._tiles = [0] * (self.width * self.width)
+            empty_row = -1
+            empty_col = -1
+            index = 0
+            for row in range(0, self.width):
+                for col in range(0, self.width):
+                    tile = tiles[row][col]
+                    if tile == self.EMPTY_TILE:
+                        empty_row = row
+                        empty_col = col
+                    self._tiles[index] = tile
+                    index += 1
+            self.empty_tile_row = empty_row
+            self.empty_tile_col = empty_col
+
+    def _init(self, width, tiles, empty_tile_row, empty_tile_col):
+        self.width = width
+        self._tiles = tiles
+        self.empty_tile_row = empty_tile_row
+        self.empty_tile_col = empty_tile_col
+
+    def get_tile(self, row, col):
+        return self._tiles[row * self.width + col]
+
+    def get_applicable_actions(self):
+        actions = set()
+        for movement in Movement:
+            new_empty_tile_row = self.empty_tile_row + movement.delta_row
+            new_empty_tile_col = self.empty_tile_col + movement.delta_col
+            if 0 <= new_empty_tile_row and new_empty_tile_row < self.width and 0 <= new_empty_tile_col and new_empty_tile_col < self.width:
+                actions.add(movement)
+        return actions
+
+    def get_action_result(self, movement):
+        new_empty_tile_row = self.empty_tile_row + movement.delta_row
+        new_empty_tile_col = self.empty_tile_col + movement.delta_col
+        new_tiles = self._tiles.copy()
+        new_tiles[self.empty_tile_row * self.width + self.empty_tile_col] = self.get_tile(new_empty_tile_row, new_empty_tile_col)
+        new_tiles[new_empty_tile_row * self.width + new_empty_tile_col] = self.EMPTY_TILE
+        result = Tiles(None)
+        result._init(self.width, new_tiles, new_empty_tile_row, new_empty_tile_col)
+        return result
+
+
+class Movement(Action, Enum):
+    UP = (-1, 0)
+    LEFT = (0, -1)
+    DOWN = (1, 0)
+    RIGHT = (0, 1)
+    
+    def __init__(self, delta_row, delta_col):
+        self.delta_row = delta_row
+        self.delta_col = delta_col
+
+
+class TilesGoalTest(GoalTest):
+    def is_goal(self, tiles):
+        last_tile_index = tiles.width * tiles.width - 1
+        for index in range(last_tile_index, 0, -1):
+            if tiles._tiles[index - 1] != index:
+                return False
+        return tiles._tiles[last_tile_index] == Tiles.EMPTY_TILE
+
+
+class NPuzzlePrinting(Printing):
+    @staticmethod
+    def print_action(movement):
+        print('move ', movement.name, sep = '', end = '')
+
+    @staticmethod
+    def print_state(tiles):
+        width = tiles.width
+        cell_length = len(str(width * width))
+        print('-', end = '')
+        for col in range(0, width):
+            print('-' * (cell_length + 3), end = '')
+        print()
+        for row in range(0, width):
+            print('|', end = '')
+            for col in range(0, width):
+                print(' ', end = '')
+                if row == tiles.empty_tile_row and col == tiles.empty_tile_col:
+                    print(' ' * cell_length, end = '')
+                else:
+                    cell = str(tiles.get_tile(row, col))
+                    print(' ' * (cell_length - len(cell)), end = '')
+                    print(cell, end = '')
+                print(' |', end = '')
+            print()
+            print('-', end = '')
+            for col in range(0, width):
+                print('-' * (cell_length + 3), end = '')
+            print()
+
+
+def BFTS_demo():
+    print('This is a demonstration of breadth-first tree search on 8-puzzle')
+    print()
+
+    initial_configuration = Tiles([
+        [ 7, 4, 2 ],
+        [ 8, 1, 3 ],
+        [ 5, 0, 6 ]
+    ])
+
+    goal_test = TilesGoalTest()
+    solution = BreadthFirstTreeSearch.find_solution(initial_configuration, goal_test)
+    NPuzzlePrinting.print_solution(solution)
+
+
+if __name__ == '__main__':
+    BFTS_demo()
+    
---
+++ search.py
@@ -0,0 +1,80 @@
+from collections import deque
+
+
+class State:
+    def get_applicable_actions(self):
+        pass
+
+    def get_action_result(self, action):
+        pass
+
+
+class Action:
+    pass
+
+
+class GoalTest:
+    def is_goal(self, state):
+        pass
+
+
+class Node:
+    def __init__(self, parent, action, state):
+        self.parent = parent
+        self.action = action
+        self.state = state
+
+
+class Printing:
+    @classmethod
+    def print_solution(cls, solution):
+        if not solution:
+            print('No solution found!')
+        else:
+            stack = deque()
+            node = solution
+            while node:
+                stack.append(node)
+                node = node.parent
+            stepNo = 0
+            while stack:
+                node = stack.pop()
+                print(stepNo, end = '')
+                stepNo += 1
+                if not node.parent:
+                    print(': start')
+                else:
+                    print(': ', end = '')
+                    cls.print_action(node.action)
+                    print()
+                print()
+                cls.print_state(node.state)
+                print()
+
+    @staticmethod
+    def print_action(action):
+        pass
+
+    @staticmethod
+    def print_state(state):
+        pass
+
+
+class BreadthFirstTreeSearch:
+    @staticmethod
+    def find_solution(initial_state, goal_test):
+        node = Node(None, None, initial_state)
+        if goal_test.is_goal(node.state):
+        	return node
+        fifo_frontier = deque()
+        fifo_frontier.append(node)
+        while fifo_frontier:
+            node = fifo_frontier.popleft()
+            for action in node.state.get_applicable_actions():
+                new_state = node.state.get_action_result(action)
+                child = Node(node, action, new_state)
+                if goal_test.is_goal(new_state):
+                    return child
+                fifo_frontier.append(child)
+        return None
+
